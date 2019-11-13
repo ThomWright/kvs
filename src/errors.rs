9b49b297168baadfc200d5920ad0bff116b69a58
Thom Wright <github@thomwright.co.uk>
2019-11-13 22:59:05 +0000

    Remove unused error

--- src/errors.rs
+++ src/errors.rs
@@ -19,10 +19,6 @@ pub enum KvsError {
     #[fail(display = "Unexpected command found in log")]
     UnexpectedCommand,
 
-    /// An unexpected key was found in the database - probably a program error
-    #[fail(display = "Unexpected key found in log")]
-    UnexpectedKey,
-
     /// An unexpected file name was found
     #[fail(display = "Unexpected file name, should be an integer")]
     UnexpectedFileName,
