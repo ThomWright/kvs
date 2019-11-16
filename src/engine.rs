3f6b197f78d185075d9b39f750285328d9cc84af
Thom Wright <github@thomwright.co.uk>
2019-11-16 12:42:59 +0000

    Improve docs for Engine interface

--- src/engine.rs
+++ src/engine.rs
@@ -3,10 +3,10 @@ use crate::Result;
 /// Interface for a simple key-value store.
 #[allow(clippy::module_name_repetitions)]
 pub trait KvsEngine {
-    #[allow(missing_docs)]
+    /// Get the value for the given key, if it exists.
     fn get(&mut self, key: String) -> Result<Option<String>>;
-    #[allow(missing_docs)]
+    /// Set the value for the given key, overwriting the previous value if it existed.
     fn set(&mut self, key: String, value: String) -> Result<()>;
-    #[allow(missing_docs)]
+    /// Remove the value for the given key. Will error if the key does not exist.
     fn remove(&mut self, key: String) -> Result<()>;
 }
