87f89deccb62860885cae2a1024cb500fccfa961
Thom Wright <github@thomwright.co.uk>
2019-11-30 11:18:10 +0000

    Add read benchmark

--- benches/benches.rs
+++ benches/benches.rs
@@ -46,6 +46,47 @@ fn write(c: &mut Criterion) {
     group.finish();
 }
 
+fn read(c: &mut Criterion) {
+    let mut group = c.benchmark_group("read");
+
+    for engine_type in &[EngineType::Kvs, EngineType::Sled] {
+        group.bench_with_input(
+            BenchmarkId::from_parameter(engine_type),
+            engine_type,
+            |b, &engine_type| {
+                b.iter_batched(
+                    || {
+                        let temp_dir =
+                            TempDir::new().expect("unable to create temporary working directory");
+                        let mut store: Box<dyn KvsEngine> = match engine_type {
+                            EngineType::Kvs => Box::new(
+                                KvStore::open(temp_dir.path()).expect("unable to open KvStore"),
+                            ),
+                            EngineType::Sled => Box::new(
+                                SledKvsEngine::open(temp_dir.path())
+                                    .expect("unable to open SledKvsEngine"),
+                            ),
+                        };
+
+                        let key = gen_random_string();
+                        let value = gen_random_string();
+
+                        store.set(key.clone(), value).unwrap();
+
+                        (store, key.clone())
+                    },
+                    |(mut store, key)| {
+                        store.get(key).unwrap();
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+
+    group.finish();
+}
+
 fn gen_random_string() -> String {
     let mut rng = rand::thread_rng();
     let length = rng.gen_range(1, 100_001);
@@ -55,5 +96,5 @@ fn gen_random_string() -> String {
         .collect::<String>()
 }
 
-criterion_group!(benches, write);
+criterion_group!(benches, write, read);
 criterion_main!(benches);
