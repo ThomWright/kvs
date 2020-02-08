4f81473ffde0a0824b77e9ceec5f439b898d4972
Thom Wright <github@thomwright.co.uk>
2020-02-08 12:31:23 +0000

    Implement rayon implementation of ThreadPool

--- src/thread_pool/rayon.rs
+++ src/thread_pool/rayon.rs
@@ -1,19 +1,26 @@
 use super::ThreadPool;
 use crate::Result;
+use rayon;
+use rayon::ThreadPoolBuilder;
 
+/// A rayon thread pool for performance comparison.
 #[allow(clippy::module_name_repetitions)]
-#[derive(Debug, Clone, Copy)]
-pub struct RayonThreadPool;
+#[derive(Debug)]
+pub struct RayonThreadPool(rayon::ThreadPool);
 
 impl ThreadPool for RayonThreadPool {
-    fn new(_: u32) -> Result<Self> {
-        unimplemented!()
+    fn new(num_threads: u32) -> Result<Self> {
+        Ok(RayonThreadPool(
+            ThreadPoolBuilder::new()
+                .num_threads(num_threads as usize)
+                .build()?,
+        ))
     }
 
-    fn spawn<F>(&self, _job: F)
+    fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        unimplemented!()
+        self.0.install(job);
     }
 }
